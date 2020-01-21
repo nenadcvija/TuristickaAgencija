4b585ad4e065b090b2320a589af07d5090628f79
okkursevi1 <okkursevi1@UC5PC10>
2020-01-21 20:12:29 +0100

    poceli login

--- TuristickaAgencija/src/service/LoginService.java
+++ TuristickaAgencija/src/service/LoginService.java
@@ -1,5 +1,21 @@
 package service;
 
+import javax.transaction.Transactional;
+
+import dao.LoginDAO;
+import model.User;
+
 public class LoginService {
+	
+	LoginDAO dao = new LoginDAO();
+
+	@Transactional
+	public User vratiUseraLogIn(String userName, String password) {
+		
+		return dao.vratiUseraLogIn(userName,password);
+	}
+	
+	
+	
 
 }
